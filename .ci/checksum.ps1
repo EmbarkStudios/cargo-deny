fc87f86397f0654aed94232547b65dec66b19e37
Jake Shadle <jake.shadle@embark-studios.com>
2019-06-27 14:41:48 +0200

    Fix script for real this time

--- .ci/checksum.ps1
+++ .ci/checksum.ps1
@@ -1,7 +1,7 @@
 param (
     [string]$filename = $(throw "-filename is required.")
 )
+
 $ErrorActionPreference="Stop"
 
-echo "filename = ${filename}"
 (Get-FileHash "${filename}").Hash | Out-File "${filename}.sha256" -NoNewline
