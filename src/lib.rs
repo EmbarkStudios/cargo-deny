c70c07f82435dde32865f9e6a34352a8bf6915c5
Jake Shadle <jake.shadle@embark-studios.com>
2019-12-19 12:27:20 +0100

    Stop supplying manifest-path

--- src/lib.rs
+++ src/lib.rs
@@ -400,9 +400,9 @@ impl AsRef<[KrateDetails]> for Krates {
 }
 
 pub fn get_all_crates<P: AsRef<Path>>(root: P) -> Result<Krates, Error> {
-    let cargo_toml = root.as_ref().join("Cargo.toml");
+    //let cargo_toml = root.as_ref().join("Cargo.toml");
     let metadata = cargo_metadata::MetadataCommand::new()
-        .manifest_path(cargo_toml)
+        //.manifest_path(cargo_toml)
         // We run cargo_metadata from the root path provided by the user
         // so that any potential .cargo/config is picked up correctly,
         // as eg. adding registries in the config that are used by one
