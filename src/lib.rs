d068512f33025ca1dbdd8800efb1d21fe64696a1
Jake Shadle <jake.shadle@embark-studios.com>
2022-05-17 14:45:16 +0200

    Remove `fix` functionality (#423)
    * Remove 'fix' functionality
    
    This functionality was only added when there was the potential for
    cargo-deny to supplant cargo-audit, which is no longer the case. This
    functionality was quite a lot of code, minimally tested and increased
    compile times (toml_edit), all for a feature that, at least
    anecdotally, is not really useful as any moderately complex dependency
    graph is unlikely to be patchable.
    
    * Remove index

--- src/lib.rs
+++ src/lib.rs
@@ -87,10 +87,8 @@ pub mod advisories;
 pub mod bans;
 mod cfg;
 pub mod diag;
-mod index;
 /// Configuration and logic for checking crate licenses
 pub mod licenses;
-pub mod manifest;
 pub mod sources;
 
 pub use cfg::{Spanned, UnvalidatedConfig};
