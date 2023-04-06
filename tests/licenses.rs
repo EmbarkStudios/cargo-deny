2db909a0287663f5442f2a223602262521eac8f3
Jake Shadle <jake.shadle@embark-studios.com>
2023-04-06 10:20:59 +0200

    Fallback to more lax parsing of SPDX expressions

--- tests/licenses.rs
+++ tests/licenses.rs
@@ -138,7 +138,7 @@ fn flags_unencountered_exceptions() {
     let cfg = tu::Config::new(
         "allow = ['MIT']
     unlicensed = 'allow'
-    exceptions = [{name='bippity-boopity-bop', allow = ['Aladdin']}]",
+    exceptions = [{name='bippity-boppity-boop', allow = ['Aladdin']}]",
     );
 
     // Override the warning to be a failure
@@ -149,6 +149,60 @@ fn flags_unencountered_exceptions() {
     let mut diags = gather_licenses_with_overrides(func_name!(), cfg, Some(overrides));
 
     diags.retain(|d| field_eq!(d, "/fields/severity", "error"));
+
+    insta::assert_json_snapshot!(diags);
+}
+
+/// Ensures that invalid SPDX expressions in strict mode can be parsed when
+/// falling back to more lax rules, but still output a warning
+#[test]
+fn lax_fallback() {
+    let mut cmd = krates::Cmd::new();
+    cmd.manifest_path("examples/04_gnu_licenses/Cargo.toml");
+
+    let krates: Krates = krates::Builder::new()
+        .build(cmd, krates::NoneFilter)
+        .unwrap();
+
+    let gatherer = licenses::Gatherer::default()
+        .with_store(store())
+        .with_confidence_threshold(0.8);
+
+    let mut files = codespan::Files::new();
+
+    let cfg = tu::Config::new(
+        "allow = ['GPL-2.0', 'LGPL-3.0']
+    unlicensed = 'deny'",
+    );
+
+    let cfg: tu::Config<crate::licenses::cfg::Config> = cfg.into();
+    let lic_cfg = {
+        let des: licenses::cfg::Config = toml::from_str(&cfg.config).unwrap();
+        let cfg_id = files.add("config.toml", cfg.config.clone());
+
+        let mut diags = Vec::new();
+        use cargo_deny::UnvalidatedConfig;
+        des.validate(cfg_id, &mut diags)
+    };
+
+    let summary = gatherer.gather(&krates, &mut files, Some(&lic_cfg));
+
+    let diags = tu::gather_diagnostics_with_files::<crate::licenses::cfg::Config, _, _>(
+        &krates,
+        "lax_fallback",
+        cfg,
+        files,
+        |ctx, _cs, tx| {
+            crate::licenses::check(
+                ctx,
+                summary,
+                diag::ErrorSink {
+                    overrides: None,
+                    channel: tx,
+                },
+            );
+        },
+    );
 
     insta::assert_json_snapshot!(diags);
 }
