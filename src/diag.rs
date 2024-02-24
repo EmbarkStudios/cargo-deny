EmbarkStudios/cargo-deny

commit 3b13cc915a2db5af93c4b5a9e5ff885447a8961b

Refactor config (#605)
This PR completely refactors the deny configuration, notably:

### `toml-span`

`toml-span` is now used for parsing toml files (currently only
deny.toml, but eventually cargo manifests as well), replacing `toml`.
This was done so that span information is _always_ available for keys
and values if we want to use it, as well as just reducing external
dependencies and build times, as serde is no longer used.

### `PackageSpec`

Specifying a package spec via the name + version combo occurs in many
locations in the config, but this has verbose in both the simple case,
where you just want to specify a crate name (eg. `[bans.deny]`), as well
as needing an entire extra key if you _do_ want to specify the version
requirement.

In addition, it was not possible to specify just a string previously due
to toml + serde making us decide between supporting plain strings for
package specs, and span information.

In all cases (and a few new ones) where the package name + version could
be used, now a simple string can be used instead, or, if you want/need
to supply additional values as a table, the `crate` key can be used
instead which follows a simple format as a single string, instead of the
separate name/version keys. `name` and `version` are still supported,
but are deprecated and will be removed in a future release.

#### Format

The string format of `PackageSpec` is quite simple:

1. No version - `<crate_name>` = `*`
1. Exact - `<crate_name>@<semver>` = `=<semver>`
2. Requirements - `<crate_name>:<semver_requirements>` =
`<semver_requirements>`

### Add `reason`, `use-instead`

Many items can now be tagged with a `reason = "<reason>"` and/or
`use-instead = "<crate_name>/<url>"` to add explanatory/helpful messages
that are surfaced with diagnostic messages. This was added to fix #578
instead of the typical fallback of relying on toml comments that might
not be part of the diagnostic span.

src/diag.rs
@@ -1,3 +1,4 @@
+pub mod general;
 mod grapher;
 mod sink;
 
@@ -6,7 +7,7 @@ pub use sink::{DiagnosticOverrides, ErrorSink};
 
 use std::{collections::HashMap, ops::Range};
 
-use crate::{Kid, Krates};
+use crate::{Kid, Krates, Span};
 pub use codespan_reporting::diagnostic::Severity;
 
 pub use codespan::FileId;
@@ -143,8 +144,6 @@ where
     }
 }
 
-pub type Span = std::ops::Range<usize>;
-
 pub struct KrateSpan {
     pub total: Span,
     pub source: usize,
@@ -194,7 +193,10 @@ impl KrateSpans {
 
             let total = span_start..sl.len() - 1;
             let source = total.end - source.len();
-            spans.push(KrateSpan { total, source });
+            spans.push(KrateSpan {
+                total: total.into(),
+                source,
+            });
 
             let mut sl2 = String::with_capacity(4 * 1024);
             let mut deps_map = HashMap::new();
@@ -218,14 +220,14 @@ impl KrateSpans {
 
     #[inline]
     pub fn label_for_index(&self, krate_index: usize, msg: impl Into<String>) -> Label {
-        Label::secondary(self.file_id, self.spans[krate_index].total.clone()).with_message(msg)
+        Label::secondary(self.file_id, self.spans[krate_index].total).with_message(msg)
     }
 
     #[inline]
     pub fn get_coord(&self, krate_index: usize) -> KrateCoord {
         KrateCoord {
             file: self.file_id,
-            span: self.spans[krate_index].total.clone(),
+            span: self.spans[krate_index].total,
         }
     }
 }
@@ -236,7 +238,7 @@ pub type CfgCoord = Coord;
 #[derive(Clone)]
 pub struct Coord {
     pub file: FileId,
-    pub span: Range<usize>,
+    pub span: Span,
 }
 
 impl Coord {
@@ -259,7 +261,7 @@ struct NodePrint {
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum DiagnosticCode {
     Advisory(crate::advisories::Code),
-    Bans(crate::bans::Code),
+    //Bans(crate::bans::Code),
     License(crate::licenses::Code),
     Source(crate::sources::Code),
 }
@@ -269,7 +271,7 @@ impl DiagnosticCode {
         use strum::IntoEnumIterator;
         crate::advisories::Code::iter()
             .map(Self::Advisory)
-            .chain(crate::bans::Code::iter().map(Self::Bans))
+            //.chain(crate::bans::Code::iter().map(Self::Bans))
             .chain(crate::licenses::Code::iter().map(Self::License))
             .chain(crate::sources::Code::iter().map(Self::Source))
     }
@@ -278,7 +280,7 @@ impl DiagnosticCode {
     pub fn as_str(self) -> &'static str {
         match self {
             Self::Advisory(code) => code.into(),
-            Self::Bans(code) => code.into(),
+            //Self::Bans(code) => code.into(),
             Self::License(code) => code.into(),
             Self::Source(code) => code.into(),
         }
@@ -299,7 +301,7 @@ impl std::str::FromStr for DiagnosticCode {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         s.parse::<crate::advisories::Code>()
             .map(Self::Advisory)
-            .or_else(|_err| s.parse::<crate::bans::Code>().map(Self::Bans))
+            //.or_else(|_err| s.parse::<crate::bans::Code>().map(Self::Bans))
             .or_else(|_err| s.parse::<crate::licenses::Code>().map(Self::License))
             .or_else(|_err| s.parse::<crate::sources::Code>().map(Self::Source))
     }

commit ea36bf292dc2c401c8ade118c823becaf86f8aef

Fix issue with bans codes as args (#615)
An issue was introduced in [#605
](https://github.com/EmbarkStudios/cargo-deny/pull/605#discussion_r1501375569)
as I had commented out code while iterating on changes and forgot to add
back this case because it wasn't covered by any tests. I've added a
snapshot test to catch it in the future, as well as added the
`deprecated` code to the list of options.

src/diag.rs
@@ -261,9 +261,10 @@ struct NodePrint {
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum DiagnosticCode {
     Advisory(crate::advisories::Code),
-    //Bans(crate::bans::Code),
+    Bans(crate::bans::Code),
     License(crate::licenses::Code),
     Source(crate::sources::Code),
+    General(general::Code),
 }
 
 impl DiagnosticCode {
@@ -271,18 +272,20 @@ impl DiagnosticCode {
         use strum::IntoEnumIterator;
         crate::advisories::Code::iter()
             .map(Self::Advisory)
-            //.chain(crate::bans::Code::iter().map(Self::Bans))
+            .chain(crate::bans::Code::iter().map(Self::Bans))
             .chain(crate::licenses::Code::iter().map(Self::License))
             .chain(crate::sources::Code::iter().map(Self::Source))
+            .chain(general::Code::iter().map(Self::General))
     }
 
     #[inline]
     pub fn as_str(self) -> &'static str {
         match self {
             Self::Advisory(code) => code.into(),
-            //Self::Bans(code) => code.into(),
+            Self::Bans(code) => code.into(),
             Self::License(code) => code.into(),
             Self::Source(code) => code.into(),
+            Self::General(code) => code.into(),
         }
     }
 }
@@ -301,9 +304,10 @@ impl std::str::FromStr for DiagnosticCode {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         s.parse::<crate::advisories::Code>()
             .map(Self::Advisory)
-            //.or_else(|_err| s.parse::<crate::bans::Code>().map(Self::Bans))
+            .or_else(|_err| s.parse::<crate::bans::Code>().map(Self::Bans))
             .or_else(|_err| s.parse::<crate::licenses::Code>().map(Self::License))
             .or_else(|_err| s.parse::<crate::sources::Code>().map(Self::Source))
+            .or_else(|_err| s.parse::<general::Code>().map(Self::General))
     }
 }
 
@@ -319,5 +323,7 @@ mod test {
                 panic!("existing code '{code}'");
             }
         }
+
+        insta::assert_debug_snapshot!(unique);
     }
 }

src/snapshots/cargo_deny__diag__test__codes_unique.snap
@@ -1,0 +1,62 @@
+---
+source: src/diag.rs
+expression: unique
+---
+{
+    "accepted",
+    "advisory-ignored",
+    "advisory-not-detected",
+    "allowed",
+    "allowed-by-organization",
+    "allowed-by-wrapper",
+    "allowed-source",
+    "banned",
+    "build-script-not-allowed",
+    "checksum-match",
+    "checksum-mismatch",
+    "default-feature-enabled",
+    "denied-by-extension",
+    "deprecated",
+    "detected-executable",
+    "detected-executable-script",
+    "duplicate",
+    "exact-features-mismatch",
+    "feature-banned",
+    "feature-not-explicitly-allowed",
+    "features-enabled",
+    "git-source-underspecified",
+    "index-cache-load-failure",
+    "index-failure",
+    "license-exception-not-encountered",
+    "license-not-encountered",
+    "missing-clarification-file",
+    "not-allowed",
+    "notice",
+    "path-bypassed",
+    "path-bypassed-by-glob",
+    "rejected",
+    "skipped",
+    "skipped-by-root",
+    "skipped-private-workspace-crate",
+    "source-not-allowed",
+    "unable-to-check-path",
+    "unknown-advisory",
+    "unknown-feature",
+    "unlicensed",
+    "unmaintained",
+    "unmatched-bypass",
+    "unmatched-glob",
+    "unmatched-organization",
+    "unmatched-path-bypass",
+    "unmatched-skip",
+    "unmatched-skip-root",
+    "unmatched-source",
+    "unmatched-wrapper",
+    "unsound",
+    "unused-wrapper",
+    "vulnerability",
+    "wildcard",
+    "yanked",
+    "yanked-ignored",
+    "yanked-not-detected",
+}