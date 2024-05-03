79ac2bac745c4e73b08e5f734ea69ebd168103b5
Jake Shadle <jake.shadle@embark-studios.com>
2024-05-03 15:51:08 +0200

    Move script to separate file

---
+++ .github/musl-cross.sh
@@ -0,0 +1,20 @@
+#!/bin/env bash
+
+root="/musl-cross"
+bin="$root/bin"
+target="aarch64-linux-musl"
+mkdir $root
+curl -fsSL "https://musl.cc/$target.tgz" | tar --strip-components=1 -C $root -xzf -
+
+MUSL_INCLUDE="$(find /musl-cross/lib/gcc/$target/ -maxdepth 2 -type d -name 'include' | head -n 1)"
+
+echo "$bin" >> "$GITHUB_PATH"
+
+{
+    echo "AR_aarch64_unknown_linux_musl=$bin/$target-ar";
+    echo "CC_aarch64_unknown_linux_musl=$bin/$target-gcc";
+    echo "CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER=$bin/$target-ld";
+    echo "CFLAGS_aarch64_unknown_linux_musl=-nostdinc -nostdlib -isystem$root/$target/include -I ${MUSL_INCLUDE}";
+} >> "$GITHUB_ENV"
+
+unset CFLAGS MUSL_INCLUDE
